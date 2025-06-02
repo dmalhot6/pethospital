075f0abcd7e37b3e121d97f807109811e00bab5d
Anandaraju Coimbatore Sivaraju <anandsjo@amazon.com>
2025-06-02 16:32:24 -0700

    dummy ui change

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -181,7 +181,7 @@ function Dashboard() {
                 Recent Activity
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                No recent activities to display.
+                No recent activities to display. Try out.
               </Typography>
             </CardContent>
           </Card>
